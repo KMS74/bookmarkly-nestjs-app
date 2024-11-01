b0f6f4c84b8e03a6189445e7397ca2038f281087
Karem Shabana <km425360@gmail.com>
2024-11-01 13:08:45 +0200

    chore: Update comment in prisma.module.ts for clarity on global module behavior

--- src/prisma/prisma.module.ts
+++ src/prisma/prisma.module.ts
@@ -1,7 +1,7 @@
 import { Global, Module } from '@nestjs/common';
 import { PrismaService } from './prisma.service';
 
-// this makes the module global and makes the PrismaService available to all modules
+// this makes the module global and makes all exported providers available to all modules in the application
 @Global()
 @Module({
   providers: [PrismaService],
